7bec24584d23ce6601021338d729bce8e272d36d
romkra <r_krafft14+github@cs.uni-kl.de>
2024-04-17 20:45:57 +0200

    polished first part added bulletpoints for 2nd and 3rd marked where examples should be included

--- pages/07 scan code.py
+++ pages/07 scan code.py
@@ -1,39 +1,56 @@
 import streamlit as st
 
-tab0, tab1  = st.tabs(["Reasoning and terminology", "toolbox"])
-with tab0: 
-      st.markdown("""
-### What are some reasons we could want to scan code for?
-- Code Quality
-- Vulnerability Detection
+st.markdown(
+"""
+### Why and for what should we scan our code?
+- Enhance Code Quality
+- automatic Vulnerability Detection
 - Dependency Management
 - Compliance and Licensing
 
 It is always important to know what your goal is during scanning. There are three we want to differentiate: 
-1. For better quality 
-2. To publish it safely to others 
-3. To publish it to many others on distribution networks like PyPi 
-               
-Code Scanners should be a stage in every CI/CD Pipeline and be done regularly and should always be taken seriously. 
-                  
-It raises the general coding hygiene, which is all the more important in a lax language like python. They can help us uphold certain coding guidelines which can help make our code easier to understand. Even if the only person reading it is ourselves in 6 months! 
-                  
-Code Scanners also search vor known vulnerabilities in you dependencies, so you do not have to do it all by yourself. Even if you are not sharing your code, chances are high you want to stay safe too.
-For this they search through different Community advisory databases and warn when a version of a used package is compromised.
-Remember, an advisory is a notification about a vulnerability. You should always think about whether you really want to ignore one.
-      
-### Vulnerability auditing software
-You run this locally as part of your release, your process, your integration tests.
-This allows you to be confident that you're not gonna deploy something with a vulnerability.
-To audit local environments install it with pip and then run pip-audit.         
-Go ahead and run it on your laptop right now and see if up anything installed with a vulnerability.
+- For ourself
+- To publish it safely to others (like via github) 
+- To publish it to many others on distribution networks like PyPi 
 
-            """)
+#### Scan your own Code for your own sake
 
-with tab1:
-      st.markdown("""
-As always in live there is a large selection of different code scanners for python.
+If we Code for ourself we still want to create the best code we can. 
 
-""")
+Advantages for Code scanners even for your self:
+- They raise the general coding hygiene, which is all the more important in a lax language like python. 
+- They help us uphold certain coding guidelines which can help make our code easier to understand. Even if the only person reading it is ourselves in 6 months! 
+- Reduce Code smells
+
+Scanners that specialize in this use case are also known as linters. Most IDEs have linters integrated but you should make sure to adapt them to your own style.
+There are many linters to choose from and there are as always debates about which is the best. But nothing is stopping you from using two or more at the same time. Some Linters that are widely used are: 
+- Pylint => do example
+- Flake8
+- pycodestyle
+- pylama (actually combines many linters and other tools)
+
+A different specialization got more into the complexity of your code. They warn you once your modules and classes reach a certain complexity threshold. This can be defined by different measures, like lines of code in a file/class/function or the depth of your loops and if-clauses. Examples are:
+- Radon 
+- Xenon => do example
+
+            
+Other Code Scanners also search for known vulnerabilities in your dependencies and other security risks. Even in a project thats only for yourself, if there is some kind of network connection involved, you want to be as secure as possible. Known security scanners are: 
+- Safety => do example
+- Bandit
+
+#### Publishing Code
+
+Publish code => automate
+baseline CI/CD
+pipeline your scanners
+set thresholds prohibit commits/merges
+
+#### Publish your Code on PyPi
+
+PyPi has a REALLY low requirement to publish
+you are all nice people, so you WANT safe code => YOU have to hold your packages to a very high standard
+(although this is beginning to change)
+
+ """)
       
 
